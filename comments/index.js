5b0a8c6af7e205cba553c8cf5f5f478b1cb5b690
feMoraes0 <fernandomoraes.lopes@gmail.com>
2023-01-25 15:14:47 -0300

    feat(comments): adds /events route

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require('cors');
+const axios = require('axios');
 
 const port = 4001;
 const app = express();
@@ -15,13 +16,31 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
+
   comments.push({ id: commentId, content });
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    }
+  }).catch(
+    error => console.log(error.message)
+  );
+
   return res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received Event: ', req.body.type);
+  res.status(204);
 });
 
 app.listen(port, () => {
