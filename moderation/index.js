65f56404bf230e4cc551086c2e906d3f900a8023
feMoraes0 <fernandomoraes.lopes@gmail.com>
2023-01-30 18:07:13 -0300

    feat(moderation): adds commentary content analysis and trigger event back

--- moderation/index.js
+++ moderation/index.js
@@ -7,8 +7,22 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+  const blockedWord = 'orange';
 
+  if(type === 'CommentCreated') {
+    const status = data.content.includes(blockedWord) ? 'rejected' : 'approved';
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        ...data,
+        status,
+      }
+    }).catch(error => console.log(error.message));
+  }
+
+  return res.status(204);
 });
 
 app.listen(port, () => {
