7bedd9a4fb4ed9189b8f13c0e63c9e1cd0d08fcc
feMoraes0 <fernandomoraes.lopes@gmail.com>
2023-01-30 18:08:06 -0300

    feat(comments): implements commentary moderation update when the event is received

--- comments/index.js
+++ comments/index.js
@@ -39,8 +39,25 @@ app.post("/posts/:id/comments", async (req, res) => {
   return res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Received Event: ', req.body.type);
+
+  const { type, data } = req.body;
+
+  if(type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find(comment => comment.id === id);
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id, postId, content, status,
+      }
+    }).catch(error => console.log(error.message));
+  }
+
   res.status(204);
 });
 
