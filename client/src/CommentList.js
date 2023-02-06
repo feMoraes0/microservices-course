793d1ed9dc938ebf70f983e327b9f11894b3c3ef
feMoraes0 <fernandomoraes.lopes@gmail.com>
2023-02-06 17:40:37 -0300

    refactor(client): updates comment content based on received status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const CommentList = ({ comments }) => {
   const renderedComment = comments.map((comment) => {
+    let content;
+    if( comment.status === 'approved' ) {
+      content = comment.content;
+    } else if( comment.status === 'pending' ) {
+      content = 'This comment is awaiting moderation';
+    } else if( comment.status === 'rejected' ) {
+      content = 'this comment has been rejected';
+    }
+
     return (
-      <li key={comment.id}>{comment.content}</li>
+      <li key={comment.id}>{content}</li>
     )
   });
 
